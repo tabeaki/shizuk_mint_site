126da6007ec684817b4b2e95b92ccf1703a27484
takaaki tabei <43052438+tabeaki@users.noreply.github.com>
2022-11-28 22:30:19 +0900

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,10 +14,10 @@ declare global {
 
 const abi = [
   "function totalSupply() public view virtual override returns (uint256)",
-  "function mint() public payable ",
+  "function mint(uint256 _mintAmount) public payable ",
   "function is_paused() public view returns (bool)",
 ]
-const contractAddress = "0xc324C8dA2aCEf319512Ccee42676B9Dc02E763D4"
+const contractAddress = "0x3951DC8dBEfA6A562122e1d031Bc7756672a6303"
 const notify = () => toast('Starting to execute a transaction')
 
 const Home: NextPage = () => {
@@ -34,32 +34,32 @@ const Home: NextPage = () => {
         await setgetAccountFlag(true);
       }
       const signer = provider.getSigner()
-      const contract = new ethers.Contract(contractAddress, abi, signer);
-
+      const contract = await new ethers.Contract(contractAddress, abi, signer);
       try{
-        const mintNumber = (await contract.totalSupply() - 4);
+        const mintNumber = (await contract.totalSupply());
+        console.log("mintNumber=" + mintNumber);
+        console.log("mintNum=" + mintNum);
         const paused = await contract.is_paused();
-        setMintNum(mintNumber)
-        setpaused(paused)  
+        setMintNum(mintNumber);
+        setpaused(paused);
       }catch(e){
         console.log(e)
       }
     }
-    setSaleInfo();
     // add Network
     const addChain = async() => {
       try{
         await (window as any).ethereum.request({
           method: 'wallet_addEthereumChain',
           params: [{
-            chainId: "0x1",
-            chainName: 'Mainnet',
+            chainId: "0x5",
+            chainName: 'Goerli',
             nativeCurrency: {
                 name: 'ETH',
                 symbol: 'ETH',
-                decimals: 1,
+                decimals: 5,
             },
-            rpcUrls: ['https://mainnet.infura.io/v3/20cd5bade2c0407da65c6811cc2a1b37'],
+            rpcUrls: ['https://goerli.infura.io/v3/20cd5bade2c0407da65c6811cc2a1b37'],
           }],
         })
         console.log("try");
@@ -70,7 +70,8 @@ const Home: NextPage = () => {
         console.log("finally");
       }
     }
-    //addChain();
+    addChain();
+    setSaleInfo();
 
   }, []);
 
@@ -80,10 +81,10 @@ const Home: NextPage = () => {
     const MetaMuskConnect = async () =>{
       const provider = new ethers.providers.Web3Provider((window as any).ethereum)
       const signer = provider.getSigner()
-      const tokenPrice = "0.25";
+      const tokenPrice = "0";
       const contract = new ethers.Contract(contractAddress, abi, signer);
       try{
-        await contract.mint({value: ethers.utils.parseEther(tokenPrice),gasLimit: 100000});
+        await contract.mint(1,{value: ethers.utils.parseEther(tokenPrice),gasLimit: 100000});
         toast('Starting to execute a transaction')
       }catch(err: any) {
         // JSONへ変換
@@ -107,7 +108,7 @@ const Home: NextPage = () => {
         <h3 className="text-xs lg:text-4xl text-white font-semibold "></h3>
         <h1 className="text-sm lg:text-2xl pt-1 text-white font-semibold ">Please Change Goeril network</h1>
         <h1 className="text-sm lg:text-2xl pt-1 text-white font-semibold "></h1>
-        <h1 className="text-base lg:text-5xl pt-1 pb-2 text-white font-semibold "> {mintNum} / 10000</h1>
+        <h1 className="text-base lg:text-5xl pt-1 pb-2 text-white font-semibold "> {mintNum} / 3000</h1>
         <h3 className="sm:text-lg lg:text-3xl pt-1 text-white font-semibold ">Price free</h3>  
         {/* ... */}
         {(!getAccountFlag) && <a href="#_" className="px-5 mt-4 mb-4 py-2.5 relative rounded group text-white font-medium inline-block">
@@ -117,7 +118,7 @@ const Home: NextPage = () => {
         <span className="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-slate-200 from-slate-500"></span>
         <span className="relative" onClick={ConnectWallet}>Connect Wallet</span>
         <Toaster /></a>}
-        {(getAccountFlag && !paused && mintNum <= 20) && <a href="#_" className="px-5 mt-4 mb-4 py-2.5 relative rounded group text-white font-medium inline-block">
+        {(getAccountFlag && !paused && mintNum <= 3000) && <a href="#_" className="px-5 mt-4 mb-4 py-2.5 relative rounded group text-white font-medium inline-block">
         <span className="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-slate-100 to-slate-500"></span>
         <span className="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-slate-100 to-slate-500"></span>
         <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-slate-100 to-slate-100"></span>
@@ -125,8 +126,8 @@ const Home: NextPage = () => {
         <span className="relative" onClick={MetaMuskConnect}>NFT Mint</span>
         <Toaster /></a>}
         
-        { (!paused && mintNum >= 20) && <h3 className="sm:text-lg lg:text-3xl pt-1 text-white font-semibold ">End of sale</h3>}
-        <br/><a className="text-sm lg:text-2xl pt-1 text-white underline" href="https://opensea.io/ja/collection/torii-project" >market palace</a>
+        { (!paused && mintNum >= 3000) && <h3 className="sm:text-lg lg:text-3xl pt-1 text-white font-semibold ">End of sale</h3>}
+        <br/><a className="text-sm lg:text-2xl pt-1 text-white underline" href="https://testnets.opensea.io/ja/collection/shizuk-test" >market palace</a>
       </div>
       
     </div>
